d6597a50f0fba89744bc661c17561e99c26ec840
chriswynn <chris@wynncodes.com>
2021-05-16 16:05:43 -0400

    [chore] add cors

--- pages/api/colors/[color].js
+++ pages/api/colors/[color].js
@@ -1,6 +1,16 @@
+import Cors from "cors";
 import { getColor } from "../../../lib/get-color";
+import { initMiddleware } from "../../../lib/init-middleware";
 
-export default function handler(req, res) {
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET"],
+  })
+);
+
+export default async function handler(req, res) {
+  await cors(req, res);
+
   const { color } = req.query;
   const randomColor = getColor(color);
   res.status(200).json({ color: randomColor });
